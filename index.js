4ef5a2b14ec58aeea067c77be4d22b02a7eead4e
Key <markpauleyjr@gmail.com>
2023-05-30 15:06:24 -0500

    updates
    -Added dealer cards
    -Added money tracking (doesn't check if you run out of money though)
    -Changed New Card to Hit
    -Added Stand. This ends the game, reveals the dealers hand (dealer hits if under 17, draws to 16), updates Cash

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
 let player = {
     cards: [],
-    cash: 200
+    total: 0,
+    cash: 200,
+    wager: 20
 }
 
 let dealer = {
-    cards: []
+    cards: [],
+    total: 0
 }
 
 //let cards = []
-let total = 0
+//let total = 0
 let hasBlackJack = false
 let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
-let totalEl = document.getElementById("total-el")
-let cardsEl = document.getElementById("cards-el")
-let playerEl = document.getElementById("player-el")
+let rules1El = document.getElementById("rules1-el")
+let rules2El = document.getElementById("rules2-el")
+let rules3El = document.getElementById("rules3-el")
+let dealerHeaderEl = document.getElementById("dealerHeader-el")
+let dealerCardsEl = document.getElementById("dealerCards-el")
+let dealerTotalEl = document.getElementById("dealerTotal-el")
+let playerHeaderEl = document.getElementById("playerHeader-el")
+let playerCardsEl = document.getElementById("playerCards-el")
+let playerTotalEl = document.getElementById("playerTotal-el")
+let playerCashEl = document.getElementById("playerCash-el")
 
-playerEl.textContent = player.name + ": $" + player.cash
+playerCashEl.textContent = "Cash: $" + player.cash
 
+
+
+//GENERATE A NEW CARD
 function getRandomCard() {
     let randomNumber = Math.floor( Math.random()*13 ) + 1
     if (randomNumber > 10) {
@@ -30,41 +43,118 @@ function getRandomCard() {
     }
 }
 
+//START THE GAME
 function startGame() {
     isAlive = true
     hasBlackJack = false
-    let firstCard = getRandomCard()
-    let secondCard = getRandomCard()
-    player.cards = [firstCard, secondCard]
-    total = firstCard + secondCard
+    let playerFirstCard = getRandomCard()
+    let playerSecondCard = getRandomCard()
+    player.cards = [playerFirstCard, playerSecondCard]
+    player.total = playerFirstCard + playerSecondCard
+    let dealerFirstCard = getRandomCard()
+    let dealerSecondCard = getRandomCard()
+    dealer.cards = [dealerFirstCard, dealerSecondCard]
+    dealer.total = dealerFirstCard + dealerSecondCard
+    dealerHeaderEl.textContent = "Dealer Cards"
+    playerHeaderEl.textContent = "Your Cards"
+    dealerTotalEl.textContent = ""
+    rules1El.textContent = ""
+    rules2El.textContent = ""
+    rules3El.textContent = ""
+    player.cash -= 20
     renderGame()
 }
 
+//RENDER THE TABLE
 function renderGame() {
-    cardsEl.textContent = "Cards: "
+    playerCardsEl.textContent = ""
     for (let i = 0; i < player.cards.length; i++) {
-        cardsEl.textContent += player.cards[i] + " "
+        playerCardsEl.textContent += "[" + player.cards[i] + "] "
     }
     
-    totalEl.textContent = "Total: " + total
-    if (total <= 20) {
-        message = "Do you want to draw a new card?"
-    } else if (total === 21) {
+    playerTotalEl.textContent = "Total: " + player.total
+    if (player.total <= 20) {
+        message = "Hit or Stand?"
+    } else if (player.total === 21) {
         message = "You've got Blackjack!"
         hasBlackJack = true
     } else {
-        message = "You're out of the game!"
+        message = "You bust!"
         isAlive = false
     }
     messageEl.textContent = message
+    playerCashEl.textContent = "Cash: $" + player.cash
+
+    dealerTotalEl.textContent = "Total: ?"
+    dealerCardsEl.textContent = "[?] "
+    for (let i = 1; i < dealer.cards.length; i++) { //start at index 1 because we don't want to show the first card
+        dealerCardsEl.textContent += "[" + dealer.cards[i] + "] "
+    }
+
+    for (let i = 0; i < dealer.cards.length; i++) {
+        console.log("Dealer Card " + i +": " + dealer.cards[i])
+    }
 }
 
-
-function newCard() {
+//DRAW A NEW CARD FOR THE PLAYER (HIT)
+function playerNewCard() {
     if (isAlive === true && hasBlackJack === false) {
         let card = getRandomCard()
-        total += card
+        player.total += card
         player.cards.push(card)
         renderGame()        
     }
+}
+
+//DRAW A NEW CARD FOR THE DEALER
+function dealerNewCard() {
+    let card = getRandomCard()
+    dealer.total += card
+    dealer.cards.push(card)
+    renderGame()        
+}
+
+//REVEAL THE DEALERS CARDS
+function revealDealerCards() {
+    dealerCardsEl.textContent = ""
+    for (let i = 0; i < dealer.cards.length; i++) { //start at index 0 because we want to show all the dealers cards now
+        dealerCardsEl.textContent += "[" + dealer.cards[i] + "] "
+    }
+    dealerTotalEl.textContent = "Total: " + dealer.total
+}
+
+//END THE GAME (STAND)
+function endGame() {
+    if (isAlive === true) {             //you have not busted
+        if (dealer.total < 17) {        //if the dealer is under 17, draw a new card ("Dealer must stand on 17")
+            do {
+                dealerNewCard()
+            }
+            while (dealer.total <16)    //keep drawing a new card if the dealer is under 16 ("must draw to 16")
+        }
+        revealDealerCards()             //Once the dealer is finished, reveal their cards
+    }
+    //if dealer busts:
+    if (dealer.total > 21) {                        //dealer has busted
+        if (player.total === 21) {                  //and you have blackjack
+            message = "Blackjack! Dealer busts!"
+            player.cash += player.wager * 2.5       //-payout equals 2.5 of your wager (your wager back, plus 150% of your wager as your winnings, "blackjack pays 3to2")
+        } else {                                    //and you don't have blackjack
+            message = "Dealer busts!"
+            player.cash += player.wager * 2         //-payout equals double your wager (your wager back, plus an equal amount as your winnings)
+        }
+    //if dealer is higher than you: lose your wager/no payout
+    } else if (dealer.total > player.total) {
+        message = "Dealer wins!"
+    //if dealer total is equal to your total:
+    } else if (dealer.total === player.total) {
+        message = "Push!"
+        player.cash += player.wager                 //payout equals wager (you get your wager back, with no winnings)
+    //if dealer total is less than your total:
+    } else {
+        message = "You win!"
+        player.cash += player.wager * 2               //payout equals double your wager (your wager back, plus an equal amount as your winnings)
+    }
+    messageEl.textContent = message
+    playerCashEl.textContent = "Cash: $" + player.cash
 }
